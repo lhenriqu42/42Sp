e2f63865dc468fe11cc75160725832f428a85d3d
LuHenriSouza <luizhapsouza@gmail.com>
2024-10-16 10:11:59 -0300

    fix split and lstmap

--- Milestone 0/Libft/ft_lstmap_bonus.c
+++ Milestone 0/Libft/ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: lhenriqu <lhenriqu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 20:54:03 by lhenriqu          #+#    #+#             */
-/*   Updated: 2024/10/15 21:36:20 by lhenriqu         ###   ########.fr       */
+/*   Updated: 2024/10/16 10:08:57 by lhenriqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,25 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*first_element;
 	t_list	*new_elem;
+	t_list	*current_elem;
 
 	if (lst == NULL)
 		return (NULL);
-	new_elem = ft_lstnew(f(lst->content));
-	if (!new_elem)
+	first_element = ft_lstnew(f(lst->content));
+	if (!first_element)
 		return (NULL);
-	first_element = new_elem;
+	current_elem = first_element;
 	lst = lst->next;
 	while (lst)
 	{
-		new_elem->next = ft_lstnew(f(lst->content));
-		if (!new_elem->next)
+		new_elem = ft_lstnew(f(lst->content));
+		if (!new_elem)
 		{
 			ft_lstclear(&first_element, del);
 			return (NULL);
 		}
-		new_elem = new_elem->next;
+		current_elem->next = new_elem;
+		current_elem = new_elem;
 		lst = lst->next;
 	}
 	return (first_element);
--- Milestone 0/Libft/ft_split.c
+++ Milestone 0/Libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: lhenriqu <lhenriqu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/13 11:29:50 by lhenriqu          #+#    #+#             */
-/*   Updated: 2024/10/15 13:16:25 by lhenriqu         ###   ########.fr       */
+/*   Updated: 2024/10/16 09:44:23 by lhenriqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,14 @@ static int	ft_count_words(const char *str, char c)
 		return (1);
 	while (*str)
 	{
-		count++;
-		while (*str != c && *str)
-			str++;
 		while (*str == c && *str)
 			str++;
+		if (*str != c && *str)
+		{
+			count++;
+			while (*str != c && *str)
+				str++;
+		}
 	}
 	return (count);
 }
@@ -59,12 +62,15 @@ static char	**fill_matrix(char **matrix, const char *str, char c)
 		len = 0;
 		while (str[len] != c && str[len])
 			len++;
-		matrix[m_word] = (char *)malloc((len + 1) * sizeof(char));
-		if (matrix[m_word] == NULL)
-			return (ft_free_matrix(matrix, m_word));
-		ft_strlcpy(matrix[m_word], str, len + 1);
-		str += len;
-		m_word++;
+		if (len > 0)
+		{
+			matrix[m_word] = (char *)malloc((len + 1) * sizeof(char));
+			if (matrix[m_word] == NULL)
+				return (ft_free_matrix(matrix, m_word));
+			ft_strlcpy(matrix[m_word], str, len + 1);
+			m_word++;
+			str += len;
+		}
 	}
 	return (matrix);
 }
@@ -73,18 +79,16 @@ char	**ft_split(char const *str, char c)
 {
 	char	**matrix;
 	int		count_words;
-	char	*strtrim;
-	char	test[2];
 
-	test[0] = c;
-	test[1] = '\0';
-	strtrim = ft_strtrim(str, test);
-	count_words = ft_count_words(strtrim, c);
+	while (*str == c && *str)
+		str++;
+	count_words = ft_count_words(str, c);
 	matrix = (char **)malloc((count_words + 1) * sizeof(char *));
 	if (matrix == NULL)
 		return (NULL);
-	matrix = fill_matrix(matrix, strtrim, c);
+	matrix = fill_matrix(matrix, str, c);
+	if (matrix == NULL)
+		return (NULL);
 	matrix[count_words] = NULL;
-	free(strtrim);
 	return (matrix);
 }
