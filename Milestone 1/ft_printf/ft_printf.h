d82f6a5444c5168d5682f1c00be8d8f8155878f8
LuHenriSouza <luizhapsouza@gmail.com>
2024-11-21 17:47:53 -0300

    printf finished

--- Milestone 1/ft_printf/ft_printf.h
+++ Milestone 1/ft_printf/ft_printf.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lhenriqu <lhenriqu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/21 17:35:55 by lhenriqu          #+#    #+#             */
+/*   Updated: 2024/11/21 17:43:42 by lhenriqu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
@@ -5,9 +17,15 @@
 # include <unistd.h>
 
 // MACROS
-# define HEXA_LOW "0123456789abcdef"
+# define DECIMAL_BASE "0123456789"
 # define HEXA_UP "0123456789ABCDEF"
+# define HEXA_LOW "0123456789abcdef"
 
-int ft_printf(const char *, ...);
+int		ft_printf(const char *format, ...);
+
+size_t	ft_putchar(char c);
+size_t	ft_putstr(char *str);
+size_t	ft_putptr(unsigned long number, char *base);
+size_t	ft_putnbr_base(long int number, char *base);
 
 #endif
